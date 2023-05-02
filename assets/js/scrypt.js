3e4f2f2210f59d0bcef64ac5baac6ea9c957ea7d
KaruDei <108537008+KaruDei@users.noreply.github.com>
2023-05-02 10:34:01 +0400

    Add files via upload

--- assets/js/scrypt.js
+++ assets/js/scrypt.js
@@ -2,6 +2,7 @@ let FoodBtn = document.getElementById('Food_btn');
 let DrinksBtn = document.getElementById('Drinks_btn');
 let TakeawayBtn = document.getElementById('Takeaway_btn');
 let imgMenu = document.getElementById('Booking');
+let menu = document.getElementById('menu');
 let buttons = document.querySelectorAll('.menu_block_button');
 
 buttons.forEach(function(btn) {
@@ -14,56 +15,19 @@ buttons.forEach(function(btn) {
 });
 
 FoodBtn.addEventListener('click', () => {
-
-	imgMenu.innerHTML = `
-		<div class="img_items">
-			<div class="d_img_item">
-				<img src="assets/img/IMAGE (2).jpg" alt="#">
-			</div>
-			<div class="d_img_item">
-				<img src="assets/img/IMAGE (3).jpg" alt="#">
-			</div>
-		</div>
-		<div class="img_items">
-			<div class="img_item">
-				<img src="assets/img/IMAGE (4).jpg" alt="#">
-			</div>
-		</div>
+	menu.innerHTML = `
+		<img src="assets/img/IMAGE (1).jpg" alt="">
 	`;
 });
 
 DrinksBtn.addEventListener('click', () => {
-	imgMenu.innerHTML = `
-		<div class="img_items">
-			<div class="d_img_item">
-				<img src="assets/img/drinks1.jpeg" alt="#">
-			</div>
-			<div class="d_img_item">
-				<img src="assets/img/drinks2.jpg" alt="#">
-			</div>
-		</div>
-		<div class="img_items">
-			<div class="img_item">
-				<img src="assets/img/drinks3.jpeg" alt="#">
-			</div>
-		</div>
+	menu.innerHTML = `
+		<img src="assets/img/menu2.png" alt="">
 	`;
 });
 
 TakeawayBtn.addEventListener('click', () => {
-	imgMenu.innerHTML = `
-		<div class="img_items">
-			<div class="d_img_item">
-				<img src="assets/img/Takeaway1.png" alt="#">
-			</div>
-			<div class="d_img_item">
-				<img src="assets/img/Takeaway2.png" alt="#">
-			</div>
-		</div>
-		<div class="img_items">
-			<div class="img_item">
-				<img src="assets/img/Takeaway3.jpg" alt="#">
-			</div>
-		</div>
+	menu.innerHTML = `
+		<img src="assets/img/menu3.jpg" alt="">
 	`;
 });
